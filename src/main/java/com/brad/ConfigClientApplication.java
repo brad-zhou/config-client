fee7d3aaf031872393e8d8006854c182928bcc42
HKHF\zhoubd <9491319096@qq.com>
2018-11-14 17:03:19 +0800

    从Eureka Server获取serviceID来调用服务

--- src/main/java/com/brad/ConfigClientApplication.java
+++ src/main/java/com/brad/ConfigClientApplication.java
@@ -3,8 +3,10 @@ package com.brad;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @Slf4j
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.brad")
 public class ConfigClientApplication {
 
